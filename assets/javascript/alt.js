44611d5d24763cc349011304e1e51f1f29419761
steveraaen <sraaen@gmail.com>
2017-01-06 16:05:16 -0500

    fully commented js, removed log commands & test files

--- assets/javascript/alt.js
+++ assets/javascript/alt.js
@@ -1,44 +1,42 @@
-var birds = ['parrot', 'duckling', 'ostrich', 'owl', 'parakeet', 'puffins', 'geese', 'swans', 'ducks', 'crows']
+var birds = ['birds','parrot', 'duckling', 'ostrich', 'owl', 'parakeet', 'puffins', 'geese', 'swans', 'ducks', 'crows']
+var birdsbg = "url('assets/images/birds.jpg')"
+/*var broadway = ['broadway','Hamilton', 'Rockettes ', 'Book of Mormon', 'West Side Story']
+var broadwaybg = "url('assets/images/broadway.jpg')"*/
+var subjects = birds;
 var gifURL;
 var curBut;
-var newBird;
-/*var pics;
-var giff;
-var purl;
-var gurl;
-var baseRes;*/
+var newButton;
 var image;
+var still, action;
 var rating;
-var greet = '<h2>I Love Birds!</h2><h5>Click on one of these birds</h5>'
-$('#greeting').append(greet)
+$("body").css("background-image", birdsbg);
+var greet = '<h2>I Love ' + subjects[0] + '!</h2><h5>Click on one of these ' + subjects[0] + '</h5>'
+var buttonGreet = 'Or enter one here for a new ' + subjects[0] + ' button'
+$('#greeting').append(greet);
+$('#nbl').append(buttonGreet);
     //=======================================================================================
     // Create buttons from array and assign them the text value of the array member
-$('#newBird').on('click', function() {
+$('#newButton').on('click', function() {
 
-    newBird = $('#newBut').val().trim();
-    /* newBird.addClass('buttons');*/
-    birds.push(newBird);
+    newButton = $('#newBut').val().trim();
+    /* newButton.addClass('buttons');*/
+    subjects.push(newButton);
     $('#btnBar').empty();
 
     renderButtons();
     return false;
 })
-
-
 function renderButtons() {
     $('#btnBar').empty();
-    for (let i = 0; i < birds.length; i++) {
-
+    for (let i = 0; i < subjects.length; i++) {
         var btn = $('<button>');
         btn.addClass('buttons')
-        btn.html(birds[i]);
+        btn.html(subjects[i]);
         $('#btnBar').append(btn);
-
     }
     return false;
 };
 renderButtons();
-
 //======================================================================================
 // Let user add a word to generate a new gif search button
 //======================================================================
@@ -47,7 +45,6 @@ $('#btnBar').on('click', '.buttons', function() {
     $('#pictures').empty();
     // Define the clicked buttons html as the search query object
     curBut = $(this).html();
-
     // Append it to the URL
     gifURL = 'http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q=' + curBut;
     $.ajax({
@@ -62,9 +59,9 @@ $('#btnBar').on('click', '.buttons', function() {
             rating.text(response.data[i].rating.toUpperCase())
             image.addClass('img-rounded');
             image.attr("src", response.data[i].images.original_still.url);
-            image.data("orig", response.data[i].images.original_still.url);
-            image.data("gif", response.data[i].images.original.url);
-            image.data("mode", "0");
+            still = image.data("orig", response.data[i].images.original_still.url);
+            action = image.data("gif", response.data[i].images.original.url);
+            image.data("mode", 0);
             image.appendTo(wrapper);    
     rating.appendTo(wrapper)
     wrapper.appendTo('#pictures')
@@ -73,16 +70,14 @@ $('#btnBar').on('click', '.buttons', function() {
 });
 $('#pictures').on('click', '.img-rounded', '.pix', function() {
   console.log(this)
-    if ($(this).data('mode') == 0) {
+    if ($(this).data('mode') === 0) {
         $(this).attr('src', $(this).data('gif'));
         $(this).data('mode', 1);
     } else {
         $(this).attr('src', $(this).data('orig'));
+        $(this).data('mode', 0);
     }
-
-
 })
 $('#gify').on('click', '.gifs', this, function() {
-
     $('#pictures').show()
 })
--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,84 +1,99 @@
-var birds = ['parrot', 'duckling', 'osprey', 'owl', 'baby+owl', 'puffins', 'geese', 'swan','ducks', 'night+heron']
+//=======================================================================================
+// Define subject array and custom bg img
+var birds = ['birds','parrot', 'duckling', 'ostrich', 'owl', 'parakeet', 'puffins', 'geese', 'swans', 'ducks', 'crows']
+var birdsbg = "url('assets/images/birds.jpg')";
+//=======================================================================================
+// Var 'subjects' acts as a container in case I decide to add additional categories (movies, sports, etc)
+var subjects = birds;
+//======================================================================================
+// Declare variables for the the url, subject buttons, new subject button, image and rating
 var gifURL;
 var curBut;
-var newBird;
-var pics;
-var giff;
+var newButton;
+var image;
+var rating;
+//======================================================================================
+// Apply custom bg image and generate heading info based on current 'subject'
+$("body").css("background-image", birdsbg);
+var greet = '<h2>I Love ' + subjects[0] + '!</h2><h5>Click on one of these ' + subjects[0] + '</h5>'
+var buttonGreet = 'Or enter one here for a new ' + subjects[0] + ' button'
+$('#greeting').append(greet);
+$('#nbl').append(buttonGreet);
 //=======================================================================================
 // Create buttons from array and assign them the text value of the array member
-    $('#newBird').on('click', function(){
-      $('#gify').hide();
-      $('#pics').show();
-
-
-    newBird = $('#newBut').val().trim();
-   /* newBird.addClass('buttons');*/
-    birds.push(newBird);
+function renderButtons() {
     $('#btnBar').empty();
-    
+    for (let i = 0; i < subjects.length; i++) {
+        var btn = $('<button>');
+        btn.addClass('buttons')
+        btn.html(subjects[i]);
+        $('#btnBar').append(btn);
+    }
+    return false;
+};
+renderButtons();
+//=======================================================================================
+// Add new subject name, push it to the array and run renderButtons function again
+$('#newButton').on('click', function() {
+    newButton = $('#newBut').val().trim();
+    subjects.push(newButton);
+    $('#btnBar').empty();
     renderButtons();
     return false;
 })
-
-
-function renderButtons(){
-    $('#btnBar').empty();
-for (let i = 0; i < birds.length;i++){
-
-        var btn = $('<button>');
-            btn.addClass('buttons')
-            btn.html(birds[i]);
-            $('#btnBar').append(btn);
-             console.log(btn);
-
-}
 renderButtons();
-return false;
-};
-
-
-//======================================================================================
-// Let user add a word to generate a new gif search button
 //======================================================================
 // Buttons are now rendered.  Logic to get gifs based on the button value
-    $('#btnBar').on('click','.buttons', function(){
-        $('#pictures').empty();
-        $('#gify').empty();
-// Define the clicked buttons html as the search query object
-     curBut = $(this).html();
-     console.log(curBut)
-// Append it to the URL
-      gifURL = 'http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q=' + curBut;
-$.ajax({
-    url: gifURL,
-    method: 'GET'
-}).done(function(response){
- console.log(response)
- 
-        for (let i = 0; i < response.data.length; i++){
-             console.log(response.data[i].images.original.url);
-        pics = $('#pictures').append('<img id = "pic_' + i + '"class = "pics" src =' + response.data[i].images.original_still.url + '>');          
-        giff = $('#gify').append('<img id = "gif_' + i + '"class = "gifs" src =' + response.data[i].images.original.url + '>');          
-            
-    } 
-  });
-});
-
-    
-    $('#pictures').on('click', '.pics', function(){
-      $(this).hide()
-console.log(this);
+$('#btnBar').on('click', '.buttons', function() {
+    $('#pictures').empty();
+//======================================================================
+// Define the clicked button's html as the search query object
+    curBut = $(this).html();
+//======================================================================
+// Append it to the URL & make the ajax call
+    gifURL = 'https://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q=' + curBut;
+    $.ajax({
+        url: gifURL,
+        method: 'GET'
+//=====================================================================
+// Define <div>s for the image container, rating and image for each item in the array
+    }).done(function(response) {
+        for (let i = 0; i < response.data.length; i++) {
+            wrapper = $("<div>").addClass("pix") 
+            rating = $("<div>").addClass("rating")
+            image = $("<img>");
+//=====================================================================
+// Prettify the text & images
+            rating.text(response.data[i].rating.toUpperCase())
+            image.addClass('img-rounded');
+//=====================================================================
+// Add img source as still or active
+            image.attr("src", response.data[i].images.original_still.url);
+            image.data("orig", response.data[i].images.original_still.url);
+            image.data("gif", response.data[i].images.original.url);
+//======================================================================
+// Add 'mode', which acts as a toggler between still and gif. Set it to 0
+            image.data("mode", 0);
+//=====================================================================
+// Append still images & ratings to the container, then append the container to existing div
+            image.appendTo(wrapper);    
+            rating.appendTo(wrapper)
+            wrapper.appendTo('#pictures')
+              }
     });
-/*    $('#gify').on('click', '.gifs', function(){
-      $(this).show()
-console.log(this);
-    });*/
-
-/*renderButtons();*/
-/*});*/
-/*var puffin1 = '<img src="http://media0.giphy.com/media/n8t6jbMD5XCCs/giphy.gif">';
-var puffin2 = '<img src="http://media1.giphy.com/media/sdSEFlOtkviBq/giphy.gif">';
-var puffin3 = '<img src="https://media2.giphy.com/media/Nx9FmaiPk873a/giphy.gif">';
-var puffin4 = '<img src="http://media4.giphy.com/media/uUE9oqZTduJRS/giphy.gif">';
-var babyGoatDog = '<img src="https://media2.giphy.com/media/mkZ78JB74isVO/giphy.gif">';*/
-
+});
+//======================================================================
+// mode 0 means still, mode 1 means active, so on 'click', if still, make active and change mode to active
+$('#pictures').on('click', '.img-rounded', '.pix', function() {
+  console.log(this)
+    if ($(this).data('mode') === 0) {
+        $(this).attr('src', $(this).data('gif'));
+        $(this).data('mode', 1);
+    } 
+//======================================================================
+// Otherwise do the opposite
+    else {
+        $(this).attr('src', $(this).data('orig'));
+        $(this).data('mode', 0);
+    }
+})
